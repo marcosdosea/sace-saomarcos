c9099aa4841741a42cc6184081d206a0eed53d4f
marcosdosea <marcosdosea@400939c0-93cb-11de-a8e9-1d0845776227>
2009-11-27 13:14:59 +0000

    Preços dos produtos

---
+++ trunk/Codigo/SACE/Telas/FrmProdutoPreco.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace SACE.Telas
+{
+    public partial class FrmProdutoPreco : Form
+    {
+        public FrmProdutoPreco()
+        {
+            InitializeComponent();
+        }
+
+        private void label10_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tb_produtoBindingNavigatorSaveItem_Click(object sender, EventArgs e)
+        {
+            this.Validate();
+            this.tb_produtoBindingSource.EndEdit();
+            this.tableAdapterManager.UpdateAll(this.saceDataSet);
+
+        }
+
+        private void FrmCalcularPreco_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'saceDataSet.tb_produto' table. You can move, or remove it, as needed.
+            this.tb_produtoTableAdapter.Fill(this.saceDataSet.tb_produto);
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
