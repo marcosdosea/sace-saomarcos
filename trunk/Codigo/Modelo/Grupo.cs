cf5d37943567d85a6f66175b050508b872bcfc74
marcosdosea <marcosdosea@400939c0-93cb-11de-a8e9-1d0845776227>
2010-04-20 20:14:34 +0000

    Nova estrutura do SACE

---
+++ trunk/Codigo/Modelo/Grupo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SACE.Modelo
+{
+    public class Grupo
+    {
+        private int codGrupo;
+
+        public int CodGrupo
+        {
+            get { return codGrupo; }
+            set { codGrupo = value; }
+        }
+        private String descricao;
+
+        public String Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+    }
+}
