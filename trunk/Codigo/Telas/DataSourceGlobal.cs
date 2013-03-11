25aaca034e07c28fa79c791dbb2d1c46628fc73f
marcosdosea <marcosdosea@400939c0-93cb-11de-a8e9-1d0845776227>
2013-03-11 17:42:06 +0000

    Versão antes da atualização de compartilhamento do contexto na classe de negpcio

---
+++ trunk/Codigo/Telas/DataSourceGlobal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using Negocio;
+
+namespace Telas
+{
+    public static class DataSourceGlobal
+    {
+
+        static BindingSource produtoBindingSouce = null;
+        static BindingSource pessoaBindingSouce = null;
+
+        public static BindingSource GetProdutoBindingSource() {
+            if (produtoBindingSouce == null)
+            {
+                produtoBindingSouce = new BindingSource();
+                produtoBindingSouce.DataSource = GerenciadorProduto.GetInstance().ObterTodosExibiveis();
+            }
+            return produtoBindingSouce;
+        }
+
+        public static BindingSource UpdateProdutoBindingSource()
+        {
+            if (produtoBindingSouce == null)
+            {
+                produtoBindingSouce = new BindingSource();
+                produtoBindingSouce.DataSource = GerenciadorProduto.GetInstance().ObterTodosExibiveis();
+            }
+            else
+            {
+                produtoBindingSouce.DataSource = GerenciadorProduto.GetInstance().ObterTodosExibiveis();
+            }
+            return produtoBindingSouce;
+        }
+
+    }
+}
